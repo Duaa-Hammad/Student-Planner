f3670e459532abb0f62b23764ecd4b002669c714
Duaa Hammad <doaahammad211@gmail.com>
2025-05-25 02:47:22 +0200

    Created Entities and Installed IMP Packages in DAL

---
+++ StudentPlanner/StudentPlanner.DAL/Entities/Assignment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPlanner.DAL.Entities
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Status { get; set; }
+        public string Priority { get; set; }
+    }
+}
---
+++ StudentPlanner/StudentPlanner.DAL/Entities/Reminder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPlanner.DAL.Entities
+{
+    public class Reminder
+    {
+        public int Id { get; set; }
+        public int? AssignmentId { get; set; } //FK
+        public int? ExamId { get; set; } //FK
+        public bool IsSent { get; set; }
+        public DateTime ReminderTime { get; set; }
+    }
+}
