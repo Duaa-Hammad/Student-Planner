9ca853207fd2a01c7525174d88b452cbd3804ce2
Duaa Hammad <doaahammad211@gmail.com>
2025-06-20 00:09:30 +0200

    Publish Website on Azure

---
+++ StudentPlanner/StudentPlanner.DAL/Migrations/20250619202251_InitialAzureMig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPlanner.DAL.Migrations
+{
+    public partial class InitialAzureMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
